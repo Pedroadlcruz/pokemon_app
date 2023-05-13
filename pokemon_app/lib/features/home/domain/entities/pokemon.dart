b5cc54be345f55d39799ffddbdc711a6a8d66978
Pedro A. De la Cruz <pedrodlcruz@coolhunterpartners.com>
2023-05-13 09:12:16 -0400

    🚧 Add pokemon entity

---
+++ pokemon_app/lib/features/home/domain/entities/pokemon.dart
@@ -0,0 +1,16 @@
+import 'package:equatable/equatable.dart';
+
+class Pokemon extends Equatable {
+  final String id;
+  final String name;
+
+  const Pokemon({required this.id, required this.name});
+
+  @override
+  String toString() {
+    return "User{id: $id, name: $name}";
+  }
+
+  @override
+  List<Object?> get props => [id, name];
+}
