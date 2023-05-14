b8ec79e9bfbee9952bf1933339ed697d33d1430e
Pedro A. De la Cruz <pedrodlcruz@coolhunterpartners.com>
2023-05-14 13:58:04 -0400

    🔧 Set up dotenv

--- pokemon_app/lib/main.dart
+++ pokemon_app/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:pokemon_app/pokemon_app.dart';
-import 'package:pokemon_app/service_locator.dart';
+import 'package:pokemon_app/service_locator.dart' as di;
 
 void main() async {
-  await setUpServiceLocator();
+  await dotenv.load();
+  await di.setUpServiceLocator();
   runApp(const PokemonApp());
 }
